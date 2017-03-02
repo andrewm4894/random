1930af4b953770a874afd5a793f90212295dc5d0
Andrew Maguire <andrewm4894@gmail.com>
2017-03-02 22:42:01 +0000

    Update get_amazon_associates_data.sh

--- get_amazon_associates_data.sh
+++ get_amazon_associates_data.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# a little script to pul last 7 day of tsv files from amazon associates report site
-
 # get list of file types to loop through
-filelist=("00-20-earnings-report-" "00-20-orders-report-" "00-20-tracking-report-")
+filelist=("-20-earnings-report-" "-20-orders-report-" "-20-tracking-report-")
 
 # get list of lobs
-loblist=("mecom")
+loblist=("mecom00_me")
 
 # set up date range
 begindate=$(date --date="-7 days" +%Y%m%d)
@@ -25,8 +23,12 @@ do
   do
     
     # loop through each lob
-    for lob in "${loblist[@]}"
+    for lobstr in "${loblist[@]}"
     do
+    
+      # set up some variables
+		  lob=`echo "$lobstr" | awk -F_ '{print $1}'`
+		  lob_clean=`echo "$lobstr" | awk -F_ '{print $2}'`
     
       # print out where we are
       echo "--------------------------------------------"
@@ -38,12 +40,12 @@ do
       filepart=$file
       filename=${lobpart}${filepart}${datepart}".tsv.gz" # the feed file we want to download
       filenameunzipped=${filename::-3}
-      bqtargettablename=${filename::-7}
+      bqtargettablename=${lob_clean}${filepart}${datepart}
       # clean target table name to be acceptable table name in bq
       bqtargettablename=`echo ${bqtargettablename} | sed 's/-/_/g'`
       retries=5 # the number of times you want to retry
       success=1 # the status of the download
-      outfilename=/home/andrew_maguire/tmp/$filename
+      outfilename=/home/tmp/$filename
       myfile=${outfilename::-3}
       rm $outfilename # remove the file if it already exists
       rm $myfile # remove the file if it already exists
@@ -69,20 +71,35 @@ do
 
       echo $myfile
       head $myfile
+      
+      # if not enough rows in file then move to next
+      linecnt=$( cat $myfile | wc -l )
+      echo '...line count is '$linecnt'...'
+      if [ ${linecnt} -le 2 ]
+      then
+      
+        echo '...SKIPPING TO NEXT AS EMPTY FILE...'
+        # kill table if already exists in bq (as is probably rubbish)
+        bq rm -f amazon.$bqtargettablename
+        continue
+        
+      else
+      
+        # load to google cloud storage
+        gsutil cp $myfile gs://my-amazon-associates-data
+        # kill table if already exists in bq
+        bq rm -f amazon.$bqtargettablename
+        # load to bigquery
+        bq load --autodetect --skip_leading_rows=1 -F '|'  amazon.$bqtargettablename gs://my-amazon-associates-data/$filenameunzipped
+        # look at table meta to make sure ok
+        bq head -n 5 amazon.$bqtargettablename
 
-      # load to google cloud storage
-      gsutil cp $myfile gs://my-amazon-associates-data
-      # kill table if already exists in bq
-      bq rm -f amazon.$bqtargettablename
-      # load to bigquery
-      bq load --autodetect --skip_leading_rows=1 -F '|'  amazon.$bqtargettablename gs://my-amazon-associates-data/$filenameunzipped
-      # look at table meta to make sure ok
-      bq head -n 5 amazon.$bqtargettablename
-
-      # clean up
-      rm $outfilename
-      rm $myfile
-      rm $myfile.clean 
+        # clean up
+        rm $outfilename
+        rm $myfile
+        rm $myfile.clean
+      
+      fi 
       
     done
   
